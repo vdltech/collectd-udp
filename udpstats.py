ce6b954e1b6774d7a48e21fb63818658cc4a51c5
Johan van den Dorpe <johan@vdltech.net>
2019-10-30 15:05:49 +0100

    Autoformat

--- udpstats.py
+++ udpstats.py
@@ -5,54 +5,57 @@ import collectd
 import signal
 from pprint import pprint
 
+
 def get_udp_stats():
-  result = {}
+    result = {}
 
-  for filename in ['/proc/net/udp', '/proc/net/udp6']:
-    with open(filename) as file:
-      for line in file:
-        parts = line.split()
+    for filename in ['/proc/net/udp', '/proc/net/udp6']:
+        with open(filename) as file:
+            for line in file:
+                parts = line.split()
 
-        # Skip Header
-        if parts[0] == 'sl':
-          continue
+                # Skip Header
+                if parts[0] == 'sl':
+                    continue
 
-        # Get hex port number and convert to decimal
-        port_hex = parts[1].split(':')[1]
-        port = int(port_hex, 16)
+                # Get hex port number and convert to decimal
+                port_hex = parts[1].split(':')[1]
+                port = int(port_hex, 16)
 
-        # Metrics
-        tx_queue, rx_queue = parts[4].split(':')
-        drops = parts[12]
+                # Metrics
+                tx_queue, rx_queue = parts[4].split(':')
+                drops = parts[12]
 
-        if port not in result:
-          result[port] = {
-            'tx_queue': 0,
-            'rx_queue': 0,
-            'drops': 0,
-          }
-        
-        result[port]['tx_queue'] += int(tx_queue)
-        result[port]['rx_queue'] += int(rx_queue)
-        result[port]['drops'] += int(drops)
+                if port not in result:
+                    result[port] = {
+                        'tx_queue': 0,
+                        'rx_queue': 0,
+                        'drops': 0,
+                    }
 
-  
-  return result
+                result[port]['tx_queue'] += int(tx_queue)
+                result[port]['rx_queue'] += int(rx_queue)
+                result[port]['drops'] += int(drops)
+
+    return result
+
 
 def configure_callback(config):
-  collectd.info('Imported udpstats module')
+    collectd.info('Imported udpstats module')
 
-def read_callback( data = None ):
-  udpstats = get_udp_stats()
 
-  for port, data in udpstats.items():
-    for metricname, value in data.items():
-      metric = collectd.Values()
-      metric.plugin = 'udp_stats'
-      metric.plugin_instance = str(port)
-      metric.type = 'counter'
-      metric.type_instance = metricname
-      metric.dispatch(values=[value])
+def read_callback(data=None):
+    udpstats = get_udp_stats()
+
+    for port, data in udpstats.items():
+        for metricname, value in data.items():
+            metric = collectd.Values()
+            metric.plugin = 'udp_stats'
+            metric.plugin_instance = str(port)
+            metric.type = 'counter'
+            metric.type_instance = metricname
+            metric.dispatch(values=[value])
+
 
 collectd.register_read(read_callback)
 collectd.register_config(configure_callback)
